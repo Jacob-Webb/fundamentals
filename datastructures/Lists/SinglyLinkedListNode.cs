52fb8214723ec744112d09159aa95afe92c6065a
Jacob Webb <jacob@jacob-webb.com>
2021-11-01 09:08:59 -0700

    initial commit

---
+++ datastructures/Lists/SinglyLinkedListNode.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataStructsAndAlgos.Lists
+{
+    public class Node<T>
+      {
+        public Node<T> Next { get; set; }
+        public T Element { get; set; }
+
+        public Node(int v)
+        {
+          Next = null;
+          Element = default(T);
+        }
+        public Node(T element)
+        {
+          Element = element;
+          Next = null;
+        }
+        public Node(T element, Node<T> nextNode)
+        {
+          Next = nextNode;
+          Element = element;
+        }
+      }
+}
